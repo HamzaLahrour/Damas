b6b1370247ae19fecee7bb72dc6144cbe1071fd6
LAHROURHamza <hamzalahrour3@gmail.com>
2024-11-18 16:54:10 +0100

    Atributos clase dama

---
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Dama.java
@@ -0,0 +1,19 @@
+package org.iesalandalus.programacion.damas.modelo;
+
+public class Dama {
+
+    private enum Color{
+        BLANCO,NEGRO;
+    }
+
+    private Color color;
+    private Posicion posicion;
+    private boolean esDamaEspecial;
+
+
+
+
+    //Crea la clase Dama cuyos atributos serán un color (del tipo enumerado Color), una posicion (de la clase Posicion) y un esDamaEspecial (del tipo booleano), con la visibilidad adecuada. Realiza un commit.
+
+
+}
