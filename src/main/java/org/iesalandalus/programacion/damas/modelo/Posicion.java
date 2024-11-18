a8b4e4b505f4db14f86cf5731ef8431064962595
LAHROURHamza <hamzalahrour3@gmail.com>
2024-11-18 14:57:20 +0100

    set y get de la clase posicion

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Posicion.java
@@ -6,5 +6,55 @@ public class Posicion {
     private char columna;
 
 
+    public int getFila() {
+        return fila;
+    }
 
+    public void setFila(int fila) {
+
+        if (fila>8 || fila<1){
+
+            throw new IllegalArgumentException("ERROR: Las filas van del 1 al 8");
+        }
+
+        this.fila = fila;
+    }
+
+    public char getColumna() {
+        return columna;
+    }
+
+    public void setColumna(char columna) {
+
+        switch (columna){
+            case 'a':
+                this.columna = columna;
+                break;
+            case 'b':
+                this.columna = columna;
+                break;
+            case 'c':
+                this.columna = columna;
+                break;
+            case 'd':
+                this.columna = columna;
+                break;
+            case 'e':
+                this.columna = columna;
+                break;
+            case 'f':
+                this.columna = columna;
+                break;
+            case 'g':
+                this.columna = columna;
+                break;
+            case 'h':
+                this.columna = columna;
+                break;
+            default:
+                throw new IllegalArgumentException("ERORR:Las columnas van de la 'a' a la 'h', no se permiten mayusculas");
+        }
+
+
+    }
 }
