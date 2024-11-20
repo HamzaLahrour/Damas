9f5e0932bdb6693ec8f1ed335a96dd6a86e01daf
LAHROURHamza <hamzalahrour3@gmail.com>
2024-11-20 18:21:04 +0100

    constructor consola

--- src/main/java/org/iesalandalus/programacion/damas/Consola.java
+++ src/main/java/org/iesalandalus/programacion/damas/Consola.java
@@ -11,14 +11,14 @@ public class Consola {
     }
 
 
-    //public static void mostrarMenu (){
-//        System.out.println("*** Menu de Opciones ***");
-//        System.out.println("1- Crear dama por defecto");
-//        System.out.println("2- Crear dama eligiendo el color");
-//        System.out.println("3- Mover dama");
-//        System.out.println("4- Salir");
+    public static void mostrarMenu (){
+        System.out.println("*** Menu de Opciones ***");
+        System.out.println("1- Crear dama por defecto");
+        System.out.println("2- Crear dama eligiendo el color");
+        System.out.println("3- Mover dama");
+        System.out.println("4- Salir");
 
-    //}
+    }
 
     //public static int elegirOpcionMenu (){
 
